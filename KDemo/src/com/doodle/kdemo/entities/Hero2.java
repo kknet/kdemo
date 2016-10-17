1c72acca97c1e9b8ea2c2fc5a4479f3cbc81a37b
kongyt <839339849@qq.com>
2016-10-17 23:27:08 +0800

    添加代码

--- KDemo/src/com/doodle/kdemo/entities/Hero2.java
+++ KDemo/src/com/doodle/kdemo/entities/Hero2.java
@@ -3,6 +3,7 @@ package com.doodle.kdemo.entities;
 import org.omg.CORBA.portable.IDLEntity;
 
 import com.badlogic.gdx.graphics.Camera;
+import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
@@ -23,11 +24,74 @@ public class Hero2 extends BaseActor {
 	private GameScene m_gameScene;
 	private Camera mainCamera;
 	
-	private TextureRegion[] idleAnim;
-	private TextureRegion[] runAnim;
-	private TextureRegion[] atk1Anim;
-	private TextureRegion[] atk2Anim;
-	private TextureRegion[] atk3Anim;
+	public static final int ANIM_IDLE_R = 1;
+	public static final int ANIM_IDLE_L = 2;
+	
+	
+	private Animation idleAnim_r;
+	private Animation runAnim_r;
+	private Animation atk1Anim_r;
+	private Animation atk2Anim_r;
+	private Animation atk3Anim_r;
+	private Animation atk4Anim_r;
+	private Animation atk5Anim_r;
+	private Animation atk6Anim_r;
+	private Animation skill_1_Anim_r;
+	private Animation skill_2_Anim_r;
+	private Animation skill_3_Anim_r;
+	private Animation skill_4_Anim_r;
+	private Animation hitAnim_r;
+	private Animation standUpAnim_r;
+	
+	private Animation idleAnim_l;
+	private Animation runAnim_l;
+	private Animation atk1Anim_l;
+	private Animation atk2Anim_l;
+	private Animation atk3Anim_l;
+	private Animation atk4Anim_l;
+	private Animation atk5Anim_l;
+	private Animation atk6Anim_l;
+	private Animation skill_1_Anim_l;
+	private Animation skill_2_Anim_l;
+	private Animation skill_3_Anim_l;
+	private Animation skill_4_Anim_l;
+	private Animation hitAnim_l;
+	private Animation standUpAnim_l;
+	
+	
+	private TextureRegion[] idleAnimTex_r;
+	private TextureRegion[] runAnimTex_r;
+	private TextureRegion[] atk1AnimTex_r;
+	private TextureRegion[] atk2AnimTex_r;
+	private TextureRegion[] atk3AnimTex_r;
+	private TextureRegion[] atk4AnimTex_r;
+	private TextureRegion[] atk5AnimTex_r;
+	private TextureRegion[] atk6AnimTex_r;
+	private TextureRegion[] skill_1_AnimTex_r;
+	private TextureRegion[] skill_2_AnimTex_r;
+	private TextureRegion[] skill_3_AnimTex_r;
+	private TextureRegion[] skill_4_AnimTex_r;
+	private TextureRegion[] hitAnimTex_r;
+	private TextureRegion[] standUpAnimTex_r;
+	
+	private TextureRegion[] idleAnimTex_l;
+	private TextureRegion[] runAnimTex_l;
+	private TextureRegion[] atk1AnimTex_l;
+	private TextureRegion[] atk2AnimTex_l;
+	private TextureRegion[] atk3AnimTex_l;
+	private TextureRegion[] atk4AnimTex_l;
+	private TextureRegion[] atk5AnimTex_l;
+	private TextureRegion[] atk6AnimTex_l;
+	private TextureRegion[] skill_1_AnimTex_l;
+	private TextureRegion[] skill_2_AnimTex_l;
+	private TextureRegion[] skill_3_AnimTex_l;
+	private TextureRegion[] skill_4_AnimTex_l;
+	private TextureRegion[] hitAnimTex_l;
+	private TextureRegion[] standUpAnimTex_l;
+	
+	private Animation currentAnimation = null;
+	private float runTime = 0;
+	private boolean isLooping = false;
 	
 	public Hero2(GameScene scene){
 		this.setTag(SV.TAG_HERO);
@@ -41,24 +105,173 @@ public class Hero2 extends BaseActor {
 		
 		TextureAtlas atlas = RM.instance().getTexAtls("game/role/role.atlas");
 		Array<Sprite> texRegions = atlas.createSprites();
+		Array<Sprite> texRegionsMirror = atlas.createSprites();
 		
+		float frame_time = 0.25f;
+		//IDLE¶¯»­
 		int idle_frames = 31;
-		idleAnim = new TextureRegion[idle_frames];
+		idleAnimTex_r = new TextureRegion[idle_frames];
+		idleAnimTex_l = new TextureRegion[idle_frames];
 		for(int i = 0; i < idle_frames; i++){
-			idleAnim[i] = texRegions.get(i);
+			idleAnimTex_r[i] = texRegions.get(i);
+			idleAnimTex_l[i] = texRegionsMirror.get(i);
+			idleAnimTex_l[i].flip(true, false);
 		}
+		idleAnim_r = new Animation(frame_time, idleAnimTex_r);
+		idleAnim_l = new Animation(frame_time, idleAnimTex_l);
 		
+		
+		//±¼ÅÜ¶¯»­
 		int run_frames = 24;
-		runAnim = new TextureRegion[run_frames];
+		runAnimTex_r = new TextureRegion[run_frames];
+		runAnimTex_l = new TextureRegion[run_frames];
 		for(int i = 0; i < run_frames; i++){
-			runAnim[i] = texRegions.get(idle_frames + i);
+			runAnimTex_r[i] = texRegions.get(idle_frames + i);
+			runAnimTex_l[i] = texRegions.get(idle_frames + i);
+			runAnimTex_l[i].flip(true, false);
 		}
+		runAnim_r = new Animation(frame_time, runAnimTex_r);
+		runAnim_l = new Animation(frame_time, runAnimTex_l);
 		
-		int atk1_frames = 13;
-		atk1Anim = new TextureRegion[atk1_frames];
+		//¹¥»÷1¶¯»­
+		int atk1_frames = 8;
+		atk1AnimTex_r = new TextureRegion[atk1_frames];
+		atk1AnimTex_l = new TextureRegion[atk1_frames];
 		for(int i = 0; i < run_frames; i++){
-			atk1Anim[i] = texRegions.get(idle_frames+ run_frames + i);
+			atk1AnimTex_r[i] = texRegions.get(idle_frames+ run_frames + i);
+			atk1AnimTex_l[i] = texRegions.get(idle_frames+ run_frames + i);
+			atk1AnimTex_l[i].flip(true, false);
 		}
+		atk1Anim_r = new Animation(frame_time, atk1AnimTex_r);
+		atk1Anim_l = new Animation(frame_time, atk1AnimTex_l);
+		
+		
+		int atk2_frames = 14;
+		atk2AnimTex_r = new TextureRegion[atk2_frames];
+		atk2AnimTex_l = new TextureRegion[atk2_frames];
+		for(int i = 0; i < atk2_frames; i++){
+			atk2AnimTex_r[i] = texRegions.get(idle_frames + run_frames + atk1_frames + i);
+			atk2AnimTex_l[i] = texRegions.get(idle_frames + run_frames + atk1_frames + i);
+			atk2AnimTex_l[i].flip(true, false);
+		}
+		atk2Anim_r = new Animation(frame_time, atk2AnimTex_r);
+		atk2Anim_l = new Animation(frame_time, atk2AnimTex_l);
+		
+		
+		int atk3_frames = 21;
+		atk3AnimTex_r = new TextureRegion[atk3_frames];
+		atk3AnimTex_l = new TextureRegion[atk3_frames];
+		for(int i = 0; i < atk3_frames; i++){
+			atk3AnimTex_r[i] = texRegions.get(idle_frames + run_frames + atk1_frames  + atk2_frames+ i);
+			atk3AnimTex_l[i] = texRegions.get(idle_frames + run_frames + atk1_frames  + atk2_frames+ i);
+			atk3AnimTex_l[i].flip(true, false);
+		}
+		atk3Anim_r = new Animation(frame_time, atk3AnimTex_r);
+		atk3Anim_l = new Animation(frame_time, atk3AnimTex_l);
+		
+		int atk4_frames = 15;
+		atk4AnimTex_r = new TextureRegion[atk4_frames];
+		atk4AnimTex_l = new TextureRegion[atk4_frames];
+		for(int i = 0; i < atk4_frames; i++){
+			atk4AnimTex_r[i] = texRegions.get(idle_frames + run_frames + atk1_frames  + atk2_frames + atk3_frames+ i);
+			atk4AnimTex_l[i] = texRegions.get(idle_frames + run_frames + atk1_frames  + atk2_frames + atk3_frames+ i);
+			atk4AnimTex_l[i].flip(true, false);
+		}
+		atk4Anim_r = new Animation(frame_time, atk4AnimTex_r);
+		atk4Anim_l = new Animation(frame_time, atk4AnimTex_l);
+		
+		
+		int atk5_frames = 29;
+		atk5AnimTex_r = new TextureRegion[atk5_frames];
+		atk5AnimTex_l = new TextureRegion[atk5_frames];
+		for(int i = 0; i < atk5_frames; i++){
+			atk5AnimTex_r[i] = texRegions.get(idle_frames + run_frames + atk1_frames  + atk2_frames + atk3_frames+ atk4_frames+ i);
+			atk5AnimTex_l[i] = texRegions.get(idle_frames + run_frames + atk1_frames  + atk2_frames + atk3_frames+ atk4_frames+ i);
+			atk5AnimTex_l[i].flip(true, false);
+		}
+		atk5Anim_r = new Animation(frame_time, atk5AnimTex_r);
+		atk5Anim_l = new Animation(frame_time, atk5AnimTex_l);
+		
+		int atk6_frames = 25;
+		atk6AnimTex_r = new TextureRegion[atk6_frames];
+		atk6AnimTex_l = new TextureRegion[atk6_frames];
+		for(int i = 0; i < atk6_frames; i++){
+			atk6AnimTex_r[i] = texRegions.get(idle_frames + run_frames + atk1_frames  + atk2_frames + atk3_frames+ atk4_frames + atk5_frames+ i);
+			atk6AnimTex_l[i] = texRegions.get(idle_frames + run_frames + atk1_frames  + atk2_frames + atk3_frames+ atk4_frames + atk5_frames+ i);
+			atk6AnimTex_l[i].flip(true, false);
+		}
+		atk6Anim_r = new Animation(frame_time, atk6AnimTex_r);
+		atk6Anim_l = new Animation(frame_time, atk6AnimTex_l);
+		
+		int normal_atk = 165;
+		int skill_1_frames = 41;
+		skill_1_AnimTex_r = new TextureRegion[skill_1_frames];
+		skill_1_AnimTex_l = new TextureRegion[skill_1_frames];
+		for(int i = 0; i < skill_1_frames; i++){
+			skill_1_AnimTex_r[i] = texRegions.get(normal_atk+ i);
+			skill_1_AnimTex_l[i] = texRegions.get(normal_atk+ i);
+			skill_1_AnimTex_l[i].flip(true, false);
+		}
+		skill_1_Anim_r = new Animation(frame_time, skill_1_AnimTex_r);
+		skill_1_Anim_l = new Animation(frame_time, skill_1_AnimTex_l);
+		
+		int skill_2_frames = 147;
+		skill_2_AnimTex_r = new TextureRegion[skill_2_frames];
+		skill_2_AnimTex_l = new TextureRegion[skill_2_frames];
+		for(int i = 0; i < skill_2_frames; i++){
+			skill_2_AnimTex_r[i] = texRegions.get(normal_atk + skill_1_frames + i);
+			skill_2_AnimTex_l[i] = texRegions.get(normal_atk + skill_1_frames + i);
+			skill_2_AnimTex_l[i].flip(true, false);
+		}
+		skill_2_Anim_r = new Animation(frame_time, skill_2_AnimTex_r);
+		skill_2_Anim_l = new Animation(frame_time, skill_2_AnimTex_l);
+		
+		int skill_3_frames = 66;
+		skill_3_AnimTex_r = new TextureRegion[skill_3_frames];
+		skill_3_AnimTex_l = new TextureRegion[skill_3_frames];
+		for(int i = 0; i < skill_3_frames; i++){
+			skill_3_AnimTex_r[i] = texRegions.get(normal_atk+ skill_1_frames + skill_2_frames + i);
+			skill_3_AnimTex_l[i] = texRegions.get(normal_atk+ skill_1_frames + skill_2_frames + i);
+			skill_3_AnimTex_l[i].flip(true, false);
+		}
+		skill_3_Anim_r = new Animation(frame_time, skill_3_AnimTex_r);
+		skill_3_Anim_l = new Animation(frame_time, skill_3_AnimTex_l);
+		
+		
+		int skill_4_frames = 54;
+		skill_4_AnimTex_r = new TextureRegion[skill_4_frames];
+		skill_4_AnimTex_l = new TextureRegion[skill_4_frames];
+		for(int i = 0; i < skill_4_frames; i++){
+			skill_4_AnimTex_r[i] = texRegions.get(normal_atk+ skill_1_frames + skill_2_frames  + skill_3_frames+ i);
+			skill_4_AnimTex_l[i] = texRegions.get(normal_atk+ skill_1_frames + skill_2_frames  + skill_3_frames+ i);
+			skill_4_AnimTex_l[i].flip(true, false);
+		}
+		skill_4_Anim_r = new Animation(frame_time, skill_4_AnimTex_r);
+		skill_4_Anim_l = new Animation(frame_time, skill_4_AnimTex_l);
+		
+		int front_frames = 473;
+		int hit_frames = 16;
+		hitAnimTex_r = new TextureRegion[hit_frames];
+		hitAnimTex_l = new TextureRegion[hit_frames];
+		for(int i = 0; i < hit_frames; i++){
+			hitAnimTex_r[i] = texRegions.get(front_frames + i);
+			hitAnimTex_l[i] = texRegions.get(front_frames + i);
+			hitAnimTex_l[i].flip(true, false);
+		}
+		hitAnim_r = new Animation(frame_time, hitAnimTex_r);
+		hitAnim_l = new Animation(frame_time, hitAnimTex_l);
+		
+		
+		int stand_up_frames = 36;
+		standUpAnimTex_r = new TextureRegion[stand_up_frames];
+		standUpAnimTex_l = new TextureRegion[stand_up_frames];
+		for(int i = 0; i < stand_up_frames; i++){
+			standUpAnimTex_r[i] = texRegions.get(front_frames + hit_frames + i);
+			standUpAnimTex_l[i] = texRegions.get(front_frames + hit_frames + i);
+			standUpAnimTex_l[i].flip(true, false);
+		}
+		standUpAnim_r = new Animation(frame_time, standUpAnimTex_r);
+		standUpAnim_l = new Animation(frame_time, standUpAnimTex_l);
 	}
 
 
@@ -99,16 +312,25 @@ public class Hero2 extends BaseActor {
 		// TODO Auto-generated method stub
 		super.draw(batch, parentAlpha);
 		
+		if(currentAnimation != null){
+			TextureRegion tex = currentAnimation.getKeyFrame(runTime, isLooping);
+			batch.draw(tex, this.getX() - mainCamera.position.x	, this.getY() - mainCamera.position.y);
+		}
+		
 	}
 
+	
+	public void playAnimation(String name){
+		this.runTime = 0;
+ //============================================================
+	}
+	
 
 	@Override
 	public boolean moveLeft() {
 		// TODO Auto-generated method stub
 		boolean rs = super.moveLeft();
 		if(rs == true){
-			this.heroSkeleton.setFlipX(true);
-			this.state.setAnimation(0, "walk", true);
 		}
 		return rs;
 	}
@@ -119,8 +341,6 @@ public class Hero2 extends BaseActor {
 		// TODO Auto-generated method stub
 		boolean rs = super.moveRight();
 		if(rs == true){
-			this.heroSkeleton.setFlipX(false);
-			this.state.setAnimation(0, "walk", true);
 		}
 		return rs;
 	}
@@ -131,7 +351,7 @@ public class Hero2 extends BaseActor {
 		// TODO Auto-generated method stub
 		boolean rs = super.moveUp();
 		if(rs == true){
-			this.state.setAnimation(0, "walk", true);
+
 		}
 		return rs;
 	}
@@ -142,7 +362,6 @@ public class Hero2 extends BaseActor {
 		// TODO Auto-generated method stub
 		boolean rs = super.moveDown();
 		if(rs == true){
-			this.state.setAnimation(0, "walk", true);
 		}
 		return rs;
 	}
@@ -153,7 +372,6 @@ public class Hero2 extends BaseActor {
 		// TODO Auto-generated method stub
 		boolean rs = super.stopLeft();
 		if(rs == true && this.y_dir == STOP){
-			this.state.setAnimation(0, "idle", true);
 		}
 		return rs;
 	}
@@ -164,7 +382,6 @@ public class Hero2 extends BaseActor {
 		// TODO Auto-generated method stub
 		boolean rs = super.stopRight();
 		if(rs == true && this.y_dir == STOP){			
-			this.state.setAnimation(0, "idle", true);
 		}
 		return rs;
 	}
@@ -175,7 +392,6 @@ public class Hero2 extends BaseActor {
 		// TODO Auto-generated method stub
 		boolean rs = super.stopUp();
 		if(rs == true && this.x_dir == STOP){
-			this.state.setAnimation(0, "idle", true);
 		}
 		return rs;
 	}
@@ -186,7 +402,7 @@ public class Hero2 extends BaseActor {
 		// TODO Auto-generated method stub
 		boolean rs = super.stopDown();
 		if(rs == true && this.x_dir == STOP){
-			this.state.setAnimation(0, "idle", true);
+
 		}
 		return rs;
 	}
@@ -197,7 +413,6 @@ public class Hero2 extends BaseActor {
 		// TODO Auto-generated method stub
 		boolean rs = super.jump();
 		if(rs == true){
-			this.state.setAnimation(1, "jump", false);
 		}
 		return rs;
 		
